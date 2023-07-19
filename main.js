8ce1bc778557eb8aa8690390f0704e05b29b4e7b
priyankacute <bhavanimurugesh2000@gmail.com>
2023-07-19 15:40:45 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,7 @@
+var arr=[45,15,70,59,300,9];
+document.write("Given array is "+[45,15,70,59,300,9]+"<br>");
+for(let i=0;i<arr.length;i++){
+		if((arr[i]%3==0)&&(arr[i]%5==0)){
+								document.write(arr[i]+"<br>");
+								}
+}
